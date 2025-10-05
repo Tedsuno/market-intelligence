ada017578e42b14c527c3345f5c889992f881362
sekkat <amine.sekkat@etu.u-paris.fr>
2025-10-05 16:30:03 +0200

    ajout

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,13 @@
 from dotenv import load_dotenv
 import os
-
-
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
+
+DATABASE_URL = None
+try:
+    import streamlit as st  # dispo en cloud
+    DATABASE_URL = st.secrets.get("DATABASE_URL", None)
+except Exception:
+    pass
+
+if not DATABASE_URL:
+    DATABASE_URL = os.getenv("DATABASE_URL")
